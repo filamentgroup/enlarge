250ad2f4efa3f65415701abac1a6df8593e899ae
Scott Jehl <scottjehl@gmail.com>
2018-03-09 12:01:05 -0600

    change class name

--- docs/init.js
+++ docs/init.js
@@ -2,7 +2,7 @@
 // Many plugins bind to an "enhance" event to init themselves on dom ready, or when new markup is inserted into the DOM
 (function( $ ){
 	$( function(){
-		$(".enlarge.inlinedemo").data("options", {
+		$(".enlarge.inline-demo").data("options", {
 			button: true,
 			hoverZoomWithoutClick: true,
 			delay: 300,
